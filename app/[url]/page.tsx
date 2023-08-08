d479b2a1e05315a299f821a0de86cea8e6ec26f7
Andrew Barba <barba@hey.com>
2023-08-08 14:45:57 -0400

    Parse incoming headers

--- app/[url]/page.tsx
+++ app/[url]/page.tsx
@@ -42,10 +42,15 @@ async function Search({ url }: { url: string }) {
   );
   apiUrl.pathname = "/request";
   apiUrl.searchParams.set("url", decodeURIComponent(url));
+  
+  const reqHeaders = Array.from(headers().entries()).reduce((acc, cur) => {
+    return { ...acc, [cur[0]]: cur[1] };
+  }, {});
+  
   const res = await (
     await fetch(apiUrl, {
       cache: "no-store",
-      headers: headers(),
+      headers: reqHeaders,
     })
   ).json();
 
